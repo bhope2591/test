84437942306bebb50cd7714041e910fb92e46584
Brandon Hope <brann8591@gmail.com>
2025-04-09 14:56:09 +0000

    x

--- models/total_hours.sql
+++ models/total_hours.sql
@@ -1,21 +1,24 @@
 {{ config(
     materialized = 'incremental',
-    incremental_strategy = 'delete+insert',
+    incremental_strategy='delete+insert',
     unique_key = "provnum || '-' || workdate",
-    pre_hook = load_raw_pbj_daily()
+    pre_hook= load_raw_pbj_daily()
 ) }}
--- Notes: SCD1-style delete+insert. Pre-hook loads raw data from S3 into Snowflake before transforming.
+-- notes: using delete+insert for scd1 type table. using prehook to pull in raw data from s3 from snowflake before transformations happen below.
 
 WITH source_data AS (
+
     SELECT 
         provnum,
         state,
         workdate,
         hours_worked
     FROM {{ source('healthcare_raw', 'RAW_PBJ_DAILY_NURSE_STAFFING') }}
+
 ),
 
 aggregated AS (
+
     SELECT
         provnum,
         state,
@@ -23,6 +26,7 @@ aggregated AS (
         SUM(hours_worked) AS total_hours_worked
     FROM source_data
     GROUP BY provnum, state, month
+
 )
 
 SELECT * FROM aggregated
