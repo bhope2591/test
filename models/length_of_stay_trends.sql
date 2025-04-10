e94ec8aeda302d2acde8aec950e34179cc2da8f8
Brandon Hope <brann8591@gmail.com>
2025-04-10 02:13:17 +0000

    ss

--- models/length_of_stay_trends.sql
+++ models/length_of_stay_trends.sql
@@ -1,29 +1,33 @@
 {{ config(
     materialized = 'incremental',
     incremental_strategy = 'delete+insert',
-    unique_key = 'reporting_period || \'-\' || stay_type',
-    pre_hook = load_raw_quality_mds()
+    unique_key = 'MEASURE_PERIOD || \'-\' || stay_type',
+    pre_hook = load_raw_quality_msr_claims()
 ) }}
 
 WITH base AS (
     SELECT
-        "CMS_Certification_Number" AS cms_certification_number,
-        "Measure_Description",
-        "Stay_Type",  -- expected values: 'Short Stay' or 'Long Stay'
-        "Reporting_Period",       -- e.g. 'Q1 2024' or a month value
-        "Measure_Value"  -- percentage or rate
-    FROM {{ source('healthcare_raw', 'RAW_NH_QUALITYMSR_MDS_OCT2024') }}
-    WHERE "Stay_Type" IS NOT NULL
+        CMS_CERTIFICATION_NUMBER AS cms_certification_number,
+        MEASURE_DESCRIPTION,
+        MEASURE_PERIOD,
+        ADJUSTED_SCORE,
+        CASE 
+            WHEN MEASURE_DESCRIPTION ILIKE '%short-stay%' THEN 'Short Stay'
+            WHEN MEASURE_DESCRIPTION ILIKE '%long-stay%' THEN 'Long Stay'
+            ELSE 'Other'
+        END AS stay_type
+    FROM {{ source('healthcare_raw', 'RAW_NH_QUALITYMSR_CLAIMS_OCT2024') }}
+    WHERE MEASURE_DESCRIPTION ILIKE '%stay%'
 ),
 
 aggregated AS (
     SELECT
-        Reporting_Period AS reporting_period,
-        Stay_Type AS stay_type,
+        MEASURE_PERIOD AS reporting_period,
+        stay_type,
         COUNT(*) AS total_measures,
-        AVG(Measure_Value) AS avg_measure_value
+        AVG(ADJUSTED_SCORE) AS avg_adjusted_score
     FROM base
-    GROUP BY reporting_period, stay_type
+    GROUP BY MEASURE_PERIOD, stay_type
 )
 
 SELECT *
